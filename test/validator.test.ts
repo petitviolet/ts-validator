824f6dd4c7cb164e8962963eb00f819f8a3747c8
petitviolet <violethero0820@gmail.com>
2022-02-22 22:21:25 +0900

    updated tests

--- test/validator.test.ts
+++ test/validator.test.ts
@@ -1,6 +1,34 @@
-import { FixedValidationResult, validate } from '../src/index'
+import { FixedValidationResult, Sync, validate, Validators } from '../src/index'
 
-describe('validate async', () => {
+type Param = {
+  name: string
+  email: string
+  age: number
+}
+const validatorsSync: Validators<Param, Sync<boolean>> = {
+  name: [[name => name.length > 0, 'must not be empty']],
+  email: [[email => /^.+@.+$/i.test(email), 'must be in email format']],
+  age: [[age => age > 0 && age < 120, 'must be between 0 and 120']],
+}
+const sleep = (seconds: number) => {
+  return new Promise(resolve => setTimeout(resolve, seconds * 1000))
+}
+const validatorsAsync: Validators<Param, Sync<boolean> | Promise<boolean>> = {
+  name: [
+    [name => name.length > 0, 'must not be empty'],
+    [name => sleep(1).then(() => !['bob', 'charlie'].includes(name)), 'must be unique'],
+  ],
+  email: [
+    [email => /^.+@.+$/i.test(email), 'must be in email format'],
+    [email => sleep(1).then(() => !['bob@example.com', 'charlie@example.com'].includes(email)), 'is already taken'],
+  ],
+  age: [
+    [age => age > 0 && age < 120, 'must be between 0 and 120'],
+    [age => sleep(1).then(() => age % 2 === 0), 'must be even'],
+  ],
+}
+
+describe('validate sync', () => {
   it('returns success results', async () => {
     type Param = {
       name: string
@@ -13,17 +41,7 @@ describe('validate async', () => {
         email: 'alice@example.com',
         age: 20,
       },
-      {
-        name: [
-          [name => name.length > 0, 'must not be empty'],
-          [async name => !['bob', 'charlie'].includes(name), 'must be unique'],
-        ],
-        email: [
-          [email => email.length > 0, 'must not be empty'],
-          [email => /^.+@.+$/i.test(email), 'must be in email format'],
-        ],
-        age: [[age => age > 0 && age < 120, 'must be between 0 and 120']],
-      }
+      validatorsSync
     )
     expect(result.valid).toBeTruthy()
   })
@@ -40,17 +58,50 @@ describe('validate async', () => {
         email: '',
         age: 120,
       },
+      validatorsSync
+    )
+    expect(result.valid).toBeFalsy()
+    if (result.valid !== false) {
+      return
+    }
+    expect(result.errors).toEqual({
+      email: ['must be in email format'],
+      age: ['must be between 0 and 120'],
+    })
+  })
+})
+
+describe('validate sync and async', () => {
+  it('returns success results', async () => {
+    type Param = {
+      name: string
+      email: string
+      age: number
+    }
+    const result = await validate<Param>(
       {
-        name: [
-          [name => name.length > 0, 'must not be empty'],
-          [async name => !['bob', 'charlie'].includes(name), 'must be unique'],
-        ],
-        email: [
-          [email => email.length > 0, 'must not be empty'],
-          [email => /^.+@.+$/i.test(email), 'must be in email format'],
-        ],
-        age: [[age => age > 0 && age < 120, 'must be between 0 and 120']],
-      }
+        name: 'alice',
+        email: 'alice@example.com',
+        age: 20,
+      },
+      validatorsAsync
+    )
+    expect(result.valid).toBeTruthy()
+  })
+
+  it('returns failure results', async () => {
+    type Param = {
+      name: string
+      email: string
+      age: number
+    }
+    const result: FixedValidationResult<Param> = await validate<Param>(
+      {
+        name: 'bob',
+        email: 'bob@example.com',
+        age: 120,
+      },
+      validatorsAsync
     )
     expect(result.valid).toBeFalsy()
     if (result.valid !== false) {
@@ -58,7 +109,7 @@ describe('validate async', () => {
     }
     expect(result.errors).toEqual({
       name: ['must be unique'],
-      email: ['must not be empty', 'must be in email format'],
+      email: ['is already taken'],
       age: ['must be between 0 and 120'],
     })
   })
